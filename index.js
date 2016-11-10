7a38676fc18bcee90ead4d33238eaae71a5cbba3
unknown <>
2016-11-10 10:49:11 +0800

    Update

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const http = require('http')
+const exec = require('exec')
 const createHandler = require('github-webhook-handler')
 const handler = createHandler({ path: '/webhook', secret: 'surmon' })
 // 上面的 secret 保持和 GitHub 后台设置的一致
+
+const port = 9988
+const projects = ['vue-blog', 'angular-admin', 'nodepress']
 
 const commands = (cmd, args, callback) => {
 	const spawn = require('child_process').spawn
 	const child = spawn(cmd, args)
 	let resp = ''
-	child.stdout.on('data', buffer => {
-		resp += buffer.toString()
-	})
-	child.stdout.on('end', () => {
-		callback(resp)
-	})
+	child.stdout.on('data', buffer => { resp += buffer.toString() })
+	child.stdout.on('end', () => { callback(resp) })
+}
+
+const projectHandler = (event, action) => {
+	const project = event.payload.repository.name
+	const branch = event.payload.ref
+	if (projects.includes(project)) {
+		console.log(`Received a ${action} event for ${project} to ${branch}`)
+		commands('sh', [`./projects/${project}.sh`], text => { console.log(text) })
+	}
 }
 
 http.createServer((req, res) => {
@@ -20,29 +29,11 @@ http.createServer((req, res) => {
 		res.statusCode = 404
 		res.end('no such location')
 	})
-}).listen(9988)
+}).listen(port)
 
 handler.on('error', err => {
 	console.error('Error:', err.message)
 })
 
-handler.on('push', event => {
-	console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref)
-		// commands('sh', ['./deploy-dev.sh'], text => { console.log(text) })
-})
-
-handler.on('commit_comment', event => {
-	console.log(event)
-	console.log('Received a push event for %s to %s', event.payload.repository.name, event.payload.ref)
-		// commands('sh', ['./deploy-dev.sh'], text => { console.log(text) })
-})
-
-/*
-handler.on('issues', event => {
-  console.log('Received an issue event for % action=%s: #%d %s',
-    event.payload.repository.name,
-    event.payload.action,
-    event.payload.issue.number,
-    event.payload.issue.title)
-})
-*/
+handler.on('push', event => { projectHandler(event, 'push') })
+handler.on('commit_comment', event => { projectHandler(event, 'commit') })
