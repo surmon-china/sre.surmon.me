afafad59a9b450a141b1466860dbc08c04f025d6
unknown <>
2016-11-10 15:40:07 +0800

    Update

---
+++ projects/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+ 
+WEB_PATH='/usr/local/wwwroot/deploy'
+WEB_USER='root'
+WEB_USERGROUP='root'
+ 
+echo "Start deployment"
+cd $WEB_PATH
+echo "pulling source code..."
+# git reset --hard origin/master
+# git clean -f
+git pull
+git checkout master
+echo "changing permissions..."
+chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
+echo "Finished."
