c91d5702899afd6cb2c8e1f8e23bc9bcd8d1901d
surmon <surmon@foxmail.com>
2017-03-10 01:48:49 +0800

    update

--- projects/surmon.me.sh
+++ projects/surmon.me.sh
@@ -13,8 +13,8 @@ git pull
 git checkout master
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
-pm2 stop nodepress
+pm2 delete nodepress
 pm2 delete surmon.me
 npm run build && pm2 start ecosystem.config.js
-pm2 start nodepress
+pm2 start /usr/local/wwwroot/nodepress/ecosystem.config.js
 echo "Finished."
