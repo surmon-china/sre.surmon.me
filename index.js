6a4f71196efcca30edf02004b2f879f934d20889
surmon-china <surmon@foxmail.com>
2018-02-27 13:04:14 +0800

    update deploy script and doDeploy

--- index.js
+++ index.js
@@ -8,16 +8,16 @@ const port = 9988
 const projects = ['surmon.me', 'angular-admin', 'nodepress', 'deploy']
 
 const projectHandler = (event, action) => {
-	const project = event.payload.repository.name
 	const branch = event.payload.ref
-	if (projects.includes(project)) {
-		console.log(new Date(), `Received a ${action} event for ${project} to ${branch}`)
+	const project = event.payload.repository.name
+	const message = event.payload.head_commit.message || ''
+	if (projects.includes(project) && message.includes('doDeploy')) {
+		console.log(new Date(), `收到一个关于项目 ${project} - ${branch} 分支的 ${action} 事件，要求服务端部署！`)
 		shell.exec(`sh ./projects/${project}.sh`, (code, stdout, stderr) => {
 		  console.log(new Date(), 'Exit code:', code)
 		  // console.log(new Date(), 'Program output:', stdout)
 		  console.log(new Date(), '执行完毕！错误信息：？', stderr)
 		})
-
 	}
 }
 
@@ -32,7 +32,6 @@ http.createServer((req, res) => {
 	  // console.log('Exit code:', code)
 	  // console.log('Program output:', stdout)
 	  // console.log('Program stderr:', stderr, stderr === '', !!stderr)
-
   })
 })
 
@@ -42,4 +41,3 @@ handler.on('error', err => {
 
 handler.on('push', event => { projectHandler(event, 'push') })
 handler.on('commit_comment', event => { projectHandler(event, 'commit') })
-
