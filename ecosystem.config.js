f50e4819ee7165628028086ecc1f32535d222b0c
surmon <surmon@foxmail.com>
2018-11-07 23:26:31 +0800

    nothing

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,7 +6,7 @@ module.exports = {
       name: "sre.surmon.me",
       max_memory_restart: "68M",
       args: [
-        "--deploy_secret=your_github_secret"
+        "--deploy_secret=surmon"
       ],
       error_file: "/usr/local/wwwlogs/sre.surmon.me/error.log",
       out_file: "/usr/local/wwwlogs/sre.surmon.me/out.log"
