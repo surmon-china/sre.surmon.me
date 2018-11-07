7d4103d663afca4aa0c5abd03e23273bbf541e69
surmon <surmon@foxmail.com>
2018-11-07 23:35:32 +0800

    update deploy service

--- services/deploy.js
+++ services/deploy.js
@@ -13,11 +13,11 @@ module.exports = (event, action) => {
   const project = event.payload.repository.name
   const message = event.payload.head_commit.message || ''
   if (projects.includes(project) && message.includes('doDeploy')) {
-    consola.info(new Date(), `收到一个关于项目 ${project} - ${branch} 分支的 ${action} 事件，要求服务端部署！`)
-    shell.exec(`sh ../projects/${project}.sh`, (code, stdout, stderr) => {
-      consola.info(new Date(), 'Exit code:', code)
+    consola.info(`收到一个关于项目 ${project} - ${branch} 分支的 ${action} 事件，要求服务端部署！`, new Date())
+    shell.exec(`sh ./projects/${project}.sh`, (code, stdout, stderr) => {
+      consola.info('Exit code:', code, new Date())
       // consola.log(new Date(), 'Program output:', stdout)
-      consola.info(new Date(), '执行完毕！错误信息：？', stderr)
+      consola.info('执行完毕！错误信息：？', new Date(), stderr)
     })
   }
 }
