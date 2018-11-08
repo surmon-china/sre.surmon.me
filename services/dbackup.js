6ba40b5d9b88c85ac58fbb2b9a79f9ac2f00c590
surmon-china <surmon@foxmail.com>
2018-11-08 11:53:29 +0800

    update

--- services/dbackup.js
+++ services/dbackup.js
@@ -3,3 +3,14 @@
  * @module sre.surmon.me/dbbackup
  * @author Surmon <https://github.com/surmon-china>
  */
+
+const qiniu = require('qiniu')
+const shell = require('shelljs')
+const consola = require('consola')
+const schedule = require('node-schedule')
+
+consola.ready('Qiniu sdk ok!')
+consola.info('compress ok!')
+consola.info('push ok!')
+consola.info('move and rename ok!')
+consola.success('Dbbackup done!')
