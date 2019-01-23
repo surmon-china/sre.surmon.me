3302cea8f4fccd79a3a5c5ae523b316961d2c223
surmon-china <surmon@foxmail.com>
2019-01-24 01:15:46 +0800

    update scripts

--- projects/nodepress.sh
+++ projects/nodepress.sh
@@ -14,5 +14,11 @@ git checkout master
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
 chmod -R 777 $WEB_PATH
+sudo pm2 stop surmon.me
+pm2 stop nodepress
+sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+npm run build
+sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
 pm2 restart nodepress
+sudo pm2 restart surmon.me
 echo "Finished."
