35292ec75950237091ab802bdd3312904ef08c97
surmon-china <surmon@foxmail.com>
2019-07-21 05:07:11 +0800

    Upgrade to v2.0.2

--- projects/nodepress.sh
+++ projects/nodepress.sh
@@ -9,7 +9,7 @@ cd $WEB_PATH
 echo "pulling source code..."
 # git reset --hard origin/master
 # git clean -f
-git pull
+git pull -f
 git checkout master
 echo "changing permissions..."
 chown -R $WEB_USER:$WEB_USERGROUP $WEB_PATH
@@ -17,6 +17,7 @@ chmod -R 777 $WEB_PATH
 sudo pm2 stop surmon.me
 pm2 stop nodepress
 sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
+rm -rf ./dist/*
 npm run build
 sync && echo 3 | sudo tee /proc/sys/vm/drop_caches
 pm2 restart nodepress
